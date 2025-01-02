b21ac8b6df36dd99844830d6b27b1ccec99ee95c
chris <chris.ehmann5@gmail.com>
2025-01-01 21:21:10 -0800

    Initial commit

---
+++ lib/livesplit.py
@@ -0,0 +1,33 @@
+import socket
+from datetime import datetime, timedelta
+
+def setup_timer(socket):
+    socket.send(b"switchto gametime\n")
+
+def start_run(socket):
+    socket.send(b"starttimer\n")
+    socket.send(b"setgametime 00:00:03\n")
+    socket.send(b"unpausegametime\n")
+
+def reset_run(socket):
+    socket.send(b"reset\n")
+
+def split(socket):
+    socket.send(b"split\n")
+
+def get_current_split(socket):
+    socket.send(b"getsplitindex\n")
+    data = int(socket.recv(4096).decode('utf-8'))
+    return data
+
+def retroactive_split(socket):
+    socket.send(b"getcurrentgametime\n")
+    data = socket.recv(4096).decode('utf-8')
+    x = data.split("\n")
+    datetime_object = datetime.strptime(x[0][:-1], '%H:%M:%S.%f')
+    change_time = (datetime_object - timedelta(seconds=2.5)).strftime('%H:%M:%S.%f')
+    original_time = datetime_object.strftime('%H:%M:%S.%f')
+
+    socket.send(b"setgametime " + str.encode(change_time) + b"\n")
+    socket.send(b"split\n")
+    socket.send(b"setgametime " + str.encode(original_time) + b"\n")
---
+++ mk64as.py
@@ -0,0 +1,137 @@
+import cv2 as cv
+import numpy as np
+import mss as mss
+import time
+import os
+import pygetwindow
+import pyautogui
+import socket
+import keras
+from keras.models import load_model
+from keras.applications.resnet50 import preprocess_input
+from lib.livesplit import start_run, reset_run, split, get_current_split, setup_timer, retroactive_split
+
+def KartSplitter64():
+
+    model = load_model('models/keras/model.keras')
+    player_select_template = cv.imread("templates/misc/reset.png")
+    player_select_template = cv.resize(player_select_template, (200, 200))
+    console_reset_template = cv.imread("templates/misc/console_reset.png")
+    console_reset_template = cv.resize(console_reset_template, (200, 200))
+    cur_track = 0
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("localhost", 16834))
+    setup_timer(s)
+
+    assets_dir = "templates/tracks"
+    list = os.listdir(assets_dir)
+    tracks_dict = {'lr' : 0, 'mmf' : 1, 'ktb' : 2, 'kd' : 3, 'tt' : 4, 'fs' : 5, 'cm' : 6, 'mr' : 7, 'ws' : 8, 'sl' : 9, 'rry' : 10, 'bc' : 11, 'dkjp' : 12, 'yv' : 13, 'bb' : 14, 'rrd' : 15}
+    tracks_templates = {}
+
+    def check_template(frame, template, threshold=0.8):
+        frame = cv.resize(frame, (200,200))
+        res = cv.matchTemplate(frame, template, cv.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+
+        if(max_val > threshold):
+            return True
+            
+        else: return False
+        
+    for img in list:
+        track_template = cv.imread(os.path.join(assets_dir, img))
+        track_template = cv.resize(track_template, (200, 200))
+        track_number = tracks_dict[img.split('.')[0]]
+        tracks_templates[track_number] = track_template
+        
+    input("Move mouse to top left corner of captured video, then press enter")
+    top_left_corner = pyautogui.position()
+    input("Move mouse to bottom right corner of captured video, then press enter")
+    bot_right_corner = pyautogui.position()
+
+    monitor = {"top": top_left_corner.y, "left": top_left_corner.x, "width": bot_right_corner.x - top_left_corner.x, "height": bot_right_corner.y - top_left_corner.y}
+    recently_split = False
+    run = False
+
+    with mss.mss() as sct:
+        while True:
+            while run:
+                frame = np.array(sct.grab(monitor))
+                frame = np.delete(frame, 3, axis=2)
+
+                curr = get_current_split(s)
+
+                if(curr == 16):
+                    run = False
+                    recently_split = False
+                    break
+
+                if(recently_split == True and curr != -1):
+                    
+                    if(check_template(frame, tracks_templates[get_current_split(s)])):
+                        recently_split = False
+                        time.sleep(10)
+                    elif(check_template(frame, player_select_template, .6) and curr != 4 and curr != 8 and curr != 12):
+                        recently_split = False
+                        run = False
+                        reset_run(s)
+                        break
+                        
+                    cv.waitKey(15)
+                    
+                else:                
+                    data = get_current_split(s)
+        
+                    ##Check if either run has reset (-1),
+                    ##or run has finished (16)
+                    if(data == -1 or data == 16):
+                        run = False
+                        break
+
+                    convert = frame
+                    validation = np.stack([preprocess_input(cv.resize(convert, (224, 224)))])
+                    pred = model.predict(validation, verbose=0)
+                    
+                    if(pred[0][0] > 0.9):
+                        print(pred)
+                        recently_split = True
+                        filename = str(pred) + ".jpg"
+                        cv.imwrite(os.path.join("testing-screenshots", filename), convert)
+                        split(s)
+
+                    else:
+                        frame = cv.resize(convert, (200,200))
+                        res = cv.matchTemplate(frame, player_select_template, cv.TM_CCOEFF_NORMED)
+                        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+
+                        if(max_val > .6):
+                            recently_split = False
+                            run = False
+                            reset_run(s)
+                            break
+
+                        if(check_template(frame, console_reset_template, .7)):
+                            retroactive_split(s)
+                            recently_split = True
+                        
+                        
+                    cv.waitKey(15)
+            else:
+                frame = np.array(sct.grab(monitor))
+                frame = np.delete(frame, 3, axis=2)
+                frame = cv.resize(frame, (200,200))
+
+                res = cv.matchTemplate(frame, tracks_templates[0], cv.TM_CCOEFF_NORMED)
+                min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+
+                if max_val > 0.8:
+                    start_run(s)
+                    run = True
+                    cur_track = 0
+                    time.sleep(5)
+                    
+                cv.waitKey(15)
+
+if __name__ == "__main__":
+    KartSplitter64()
